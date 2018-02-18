fda85ead9a6d18e87836832c5101c6c9e8dfadbc
Mayank Jain <mayank.j@sigmainfo.net>
2018-02-18 18:31:21 +0530

    Added cors package and allowed cors for localhost:3000

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 
 var routes = require('./routes');
 
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  }
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(cors(corsOptions));
 
 app.use('/', routes);
 
